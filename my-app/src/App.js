930f483b9c02613106ba5e13f9052c8cd6b5fc46
Yazan Awad <awadyazan25@gmail.com>
2021-11-23 20:43:09 -0800

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+// 1) Create a list item component
+// 2) use https://reqres.in/api/users?page=2 to fetch users
+// 3) create users components
+// 4) display the user components in a list in a list
+
+function App() {
+  const getUsers = () => {
+    // fetch users here
+  };
+  return <h1>BNB Assessment</h1>;
+}
+
+export default App;
